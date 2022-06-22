0aa46c846448e78cfd9e1832a6ebf1a6b1ce4c17
Steffan Perera <steffanperera1997@gmail.com>
2022-06-23 01:22:46 +0530

    commit

--- TestApp/App.js
+++ TestApp/App.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, Text, TextInput, View} from 'react-native';
 
 // root component
 export default function App() {
+  const [name, setName] = useState('sam');
+  const [age, setAge] = useState(30);
+
   return (
     <View style={styles.container}>
-      <Text></Text>
-      <Text></Text>
+      <View style={styles.inputContainer}>
+        <Text>Enter text:</Text>
+        <TextInput style={styles.textInput} placeholder="Eg: saman" />
+      </View>
+      <Text>
+        name: {name}, age: {age}
+      </Text>
     </View>
   );
 }
@@ -16,5 +24,16 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'red',
+    padding: 8,
+    margin: 10,
+    width: 200,
   },
 });
